3a331d1327228a3d24d255736d0cfd8248cc6f56
Melissa Kaulfuss <melissa@icelab.com.au>
2014-03-01 19:23:47 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickBlog::Application.config.secret_key_base = 'ce8f110240ea323adccbd42240e37d11acef66de9d9398ac4f21a011953aa947533ab7b8a899f9f765cf4393d413db67b1b9c65e3ac98d7b300143f2bb5adb96'
